7359884d8b6a62a2768c4094b8677626ac7549a7
elyas-bhy <elyas.bhy@gmail.com>
2014-02-21 16:58:58 +0100

    Fixed game initialization

--- source/platform/src/my/first/game/AbstractPygmyGame.java
+++ source/platform/src/my/first/game/AbstractPygmyGame.java
@@ -1,13 +1,15 @@
 package my.first.game;
 
 import gameframework.game.Game;
+import gameframework.game.GameDefaultImpl;
 
 public abstract class AbstractPygmyGame implements PygmyGame {
 	
 	private Game game;
 	
 	public AbstractPygmyGame() {
-		game = new PygmyGameImpl();
+		//game = new PygmyGameImpl();
+		game = new GameDefaultImpl();
 	}
 
 	public Game getGame() {
--- source/platform/src/my/first/game/DemoPygmyGame.java
+++ source/platform/src/my/first/game/DemoPygmyGame.java
@@ -1,7 +1,6 @@
 package my.first.game;
 
 import gameframework.game.Game;
-import gameframework.game.GameDefaultImpl;
 import gameframework.game.GameLevel;
 import gameframework.game.OverlapRulesApplier;
 
@@ -11,40 +10,31 @@ import java.util.ArrayList;
 import pacman.rule.PacmanMoveBlockers;
 import pacman.rule.PacmanOverlapRules;
 
-public class DemoPygmyGame extends AbstractPygmyGame {
-	
+public class DemoPygmyGame {
+
 	public static void main(String[] args) {
-		GameDefaultImpl g = new GameDefaultImpl();
-		g.setPlayers(1, 10);
-		g.setBoardDimensions(10, 10);
-		g.setTitle("DemoGame");
-		
-		ArrayList<GameLevel> levels = new ArrayList<GameLevel>();
-		
-		OverlapRulesApplier rules = new PacmanOverlapRules(new Point(224, 272), new Point(224, 240));
-		PygmyGameLevel level1 = new DemoLevel(g, rules, new PacmanMoveBlockers());
-		
-		levels.add(level1); // only one level is available at this time
-		g.setLevels(levels);
-		//g.start();
-		level1.start();
+		DemoGame game = new DemoGame();
+		game.initGame();
 	}
 
-	@Override
-	public void initGame() {
-		Game game = getGame();
-		game.setPlayers(1, 10);
-		game.setBoardDimensions(10, 10);
-		game.setTitle("DemoGame");
-		
-		ArrayList<GameLevel> levels = new ArrayList<GameLevel>();
-		
-		OverlapRulesApplier rules = new PacmanOverlapRules(new Point(224, 272), new Point(224, 240));
-		PygmyGameLevel level1 = new DemoLevel(game, rules, new PacmanMoveBlockers());
-		
-		levels.add(level1); // only one level is available at this time
-		game.setLevels(levels);
-		//game.start();
+	private static class DemoGame extends AbstractPygmyGame {
+
+		@Override
+		public void initGame() {
+			Game game = getGame();
+			game.setPlayers(1, 10);
+			game.setBoardDimensions(10, 10);
+			game.setTitle("DemoGame");
+
+			ArrayList<GameLevel> levels = new ArrayList<GameLevel>();
+
+			OverlapRulesApplier rules = new PacmanOverlapRules(new Point(224, 272), new Point(224, 240));
+			PygmyGameLevel level1 = new DemoLevel(game, rules, new PacmanMoveBlockers());
+			levels.add(level1); // only one level is available at this time
+			
+			game.setLevels(levels);
+			level1.start();
+		}
 	}
 
 }
