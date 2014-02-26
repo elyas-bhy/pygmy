b65658b70647af23db734c89bbd575fbcfc5cb04
david-co <david.pineda@u-bordeaux1.fr>
2014-02-26 20:12:36 +0100

    Put GameBoardView and EntityView in the same layout

--- source/pygmy-app/src/com/dev/pygmy/GameBoardInterfaceActivity.java
+++ source/pygmy-app/src/com/dev/pygmy/GameBoardInterfaceActivity.java
@@ -1,13 +1,42 @@
 package com.dev.pygmy;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
+@SuppressLint("ResourceAsColor")
 public class GameBoardInterfaceActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(new GameBoardView(getApplicationContext()));;
-    }    
+	static final String TAG = "Pygmy";
+	
+	private GameBoardView gameBoardView = null;
+	private EntityView entityView = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		FrameLayout mainLayout = createMainLayout();
+		setContentView(mainLayout);
+		
+		gameBoardView = new GameBoardView(getApplicationContext());
+		mainLayout.addView(gameBoardView);
+		
+		entityView = new EntityView(this);
+		mainLayout.addView(entityView);
+	}
+	
+	private FrameLayout createMainLayout() {
+		FrameLayout mainLayout = new FrameLayout(this);
+		LayoutParams gerenalLayoutParams = new LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT);
+		mainLayout.setLayoutParams(gerenalLayoutParams);
+		mainLayout.setBackgroundColor(R.color.blue);
+		
+		return mainLayout;
+	}
 }
