59b973ceb351f059cd8e25c55c9b25557f7d8c08
david-co <david.pineda@u-bordeaux1.fr>
2014-03-03 12:34:08 +0100

    Fix problem with Null Pointers

--- source/pygmy-app/src/com/dev/pygmy/EntityView.java
+++ source/pygmy-app/src/com/dev/pygmy/EntityView.java
@@ -29,10 +29,10 @@ import android.view.View;
  */
 public class EntityView extends View {
 	private String TAG="EntityView";
-	private Entity[] entities; // array that holds the entities
-	private int entityID = 0; // variable to know what entity is being dragged
+	private Entity[] entities;		// array that holds the entities
+	private int entityID = 0;		// variable to know what entity is being dragged
 	private boolean initial = true;
-	
+
 	/**
 	 * Constructor by default. 
 	 * @param context
@@ -62,21 +62,22 @@ public class EntityView extends View {
 		// setting the start point for the entities
 		if (initial) {
 			initial = false;
-			// FIXME: fix this bad hack
+			// FIXME: fix this bad hack, getRectCoord is a static method.
 			int [][] coordXY = GameBoardView.getRectCoord();
 
-			for (int index=0; index<entities.length; index++) {
-				if (entities[index] != null) {
-					int id = entities[index].getID();
-					entities[index].setX(coordXY[id][0]);
-					entities[index].setY(coordXY[id][1]);			
+			for (int id=1; id<entities.length; id++) {
+				if (entities[id] != null) {
+					entities[id].setX(coordXY[id][0]);
+					entities[id].setY(coordXY[id][1]);
 				}
 			}
 		}
 
 		//draw the entity on the canvas
 		for (Entity ent : entities) {
-			canvas.drawBitmap(ent.getBitmap(), ent.getX(), ent.getY(), null);
+			if (ent != null) {
+				canvas.drawBitmap(ent.getBitmap(), ent.getX(), ent.getY(), null);
+			}
 		}
 	}
 
@@ -84,10 +85,10 @@ public class EntityView extends View {
 	 * Events when touching the screen
 	 */
 	public boolean onTouchEvent(MotionEvent event) {
-		int eventaction = event.getAction(); 
+		int eventaction = event.getAction();
 
-		int X = (int)event.getX(); 
-		int Y = (int)event.getY(); 
+		int X = (int)event.getX();
+		int Y = (int)event.getY();
 
 		switch (eventaction) { 
 
@@ -95,24 +96,28 @@ public class EntityView extends View {
 			entityID = 0;
 			for (Entity ent : entities) {
 				// check all the bounds of the entity
-				if (X > ent.getX() && X < ent.getX()+50 && Y > ent.getY() && Y < ent.getY()+50){
-					entityID = ent.getID();
-					break;
+				if (ent != null) {
+					if (X > ent.getX() && 
+							X < ent.getX()+50 &&
+								Y > ent.getY() && 
+									Y < ent.getY()+50) {
+						entityID = ent.getID();
+						break;
+					}
 				}
 			}
-			break; 
-
+			break;
 
 		case MotionEvent.ACTION_MOVE:   // touch drag with the entity
 			// move the entities the same as the finger
 			if (entityID > 0) {
-				entities[entityID-1].setX(X-25);
-				entities[entityID-1].setY(Y-25);
+				entities[entityID].setX(X-25);
+				entities[entityID].setY(Y-25);
 			}
 
 			break; 
 
-		case MotionEvent.ACTION_UP: 
+		case MotionEvent.ACTION_UP:
 			break; 
 		} 
 		// redraw the canvas
